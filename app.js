512302806fad74cb24abcfc24ddcaabd84439d10
cuzzea <cuzzea@gmail.com>
2011-11-23 12:16:38 +0200

    port 5000

--- app.js
+++ app.js
@@ -33,5 +33,5 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(44266);
+app.listen(5000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
